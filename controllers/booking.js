438c8ac49dadd9f36b1776aeb8323074d972f1fc
Tushar Sonawane <tusharsonawane1903@gmail.com>
2026-03-30 21:23:40 +0530

    Bug Fix:Extra Listing line removed

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const Listing = require("../models/listing.js");
 const Booking = require("../models/booking.js");
-const Listing = require("../models/listing");
 
 module.exports.bookHotel= async (req, res) => {
     const listing = await Listing.findById(req.params.id);
