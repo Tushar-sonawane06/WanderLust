2de59f347db8dee1843de10d62341bbe5d9a3b42
Tushar Sonawane <tusharsonawane1903@gmail.com>
2026-04-01 23:38:17 +0530

    Bug Fix:Improved UI of Booking page

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const sessionOptions= {
     resave:false,
     saveUninitialized: false,
     cookie:{
-        secure:true,
+        secure:false,
         expires:Date.now() + 7*24*60*60*1000,   
         maxAge: 7*24*60*60*1000,
         httpOnly:true,
